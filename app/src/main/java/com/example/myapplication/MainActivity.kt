88fa10ef20a056a4a6a7b6b2094995c4725f9505
kwakkun2002 <kwakkun2002@gmail.com>
2021-04-11 00:06:44 +0900

    버튼 클릭리스너 만듬
    버튼 클릭리스너 만듬

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import com.example.myapplication.databinding.ActivityMainBinding
 
 const val EXTRA_MESSAGE = "com.example.mtfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        binding.button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                sendMessage(view)
+            }
+        }
+        )
     }
 
     fun sendMessage(view: View){
